d35dd9701ddad50df8d9a2f66688303706896f5b
Дмитрий <105663726+YgamiJS@users.noreply.github.com>
2023-08-29 14:05:41 +0300

    fix padding

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
     const todos = useAppSelector((state) => state.todo.todos);
 
     return (
-        <div>
+        <div className="px-4">
             <h1 className="text-gray-200 text-5xl text-center">React Todo</h1>
             <Controls />
             <TodoList todos={todos} />
